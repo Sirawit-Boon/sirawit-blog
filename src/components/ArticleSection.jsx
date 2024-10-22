9386f98178a6aac7cb63641435e2cdf576963293
Sirawit-Boon <ter.sirawit1997@gmail.com>
2024-10-22 16:00:11 +0700

    fixed: clean code on button and blogcard

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -4,27 +4,28 @@ import { Search } from "lucide-react";
 import { blogPosts } from "../data/blogPost";
 
 export function ArticleSection() {
+  const categories = ["Highlight", "Cat", "Inspiration", "General"];
   return (
     <section className="latest-article gap-[80px] flex bg-[#F9F8F6] flex-col justify-center items-center">
       <div className="article-header gap-[32px] h-[144px] flex flex-col w-full">
         <h3 className="text-header text-[24px] text-left">Latest articles</h3>
         <div className="article-tabbar flex w-full h-[80px] justify-between items-center bg-[#EFEEEB] rounded-xl px-10">
-          <div className="article-tabbar-list gap-[8px]">
-            <Button variant="secondary" className="bg-[#DAD6D1]">
-              Hightlight
-            </Button>
-            <Button variant="secondary" className="bg-[#EFEEEB]">
-              Cat
-            </Button>
-            <Button variant="secondary" className="bg-[#EFEEEB]">
-              Inspiration
-            </Button>
-            <Button variant="secondary" className="bg-[#EFEEEB]">
-              General
-            </Button>
+          <div className="hidden md:flex space-x-2">
+            <button className="px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[#DAD6D1]">
+              Highlight
+            </button>
+            {categories.slice(1).map((categorie, index) => {
+              return <button key={index} className="px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[muted]">
+                {categorie}
+              </button>;
+            })}
           </div>
           <div className="input-search relative w-[360px]">
-            <Input type="text" placeholder="Search" className="w-full rounded-xl" />
+            <Input
+              type="text"
+              placeholder="Search"
+              className="w-full rounded-xl"
+            />
             <Search
               className="absolute left-[325px] top-1/4 text-gray-400"
               size={20}
@@ -33,7 +34,7 @@ export function ArticleSection() {
         </div>
       </div>
       <div className="blog-card-content grid grid-cols-2 gap-x-4 gap-y-4 max-w-[1200px]">
-        <div>
+        {/* <div>
           <BlogCard
             title={blogPosts[0].title}
             image={blogPosts[0].image}
@@ -88,35 +89,55 @@ export function ArticleSection() {
             author={blogPosts[5].author}
             date={blogPosts[5].date}
           />
-        </div>
+        </div> */}
+        {blogPosts.map((key, index) => {
+          return (
+            <BlogCard 
+            key={index}
+            image={key.image}
+            title={key.title}
+            category={key.category}
+            description={key.description}
+            date={key.date}
+            author={key.author}
+            />
+          );
+        })}
       </div>
     </section>
   );
 }
 
-export function BlogCard(props) {
+export function BlogCard({
+  title,
+  image,
+  category,
+  description,
+  author,
+  date,
+}) {
   return (
     <div className="flex flex-col gap-4">
       <a href="#" className="relative h-[212px] sm:h-[360px]">
         <img
           className="w-full h-full object-cover rounded-md"
-          src={props.image}
-          alt={props.title}
+          src={image}
+          alt={title}
         />
       </a>
       <div className="flex flex-col">
         <div className="flex">
           <span className="bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-green-600 mb-2">
-            {props.category}
+            {category}
           </span>
         </div>
         <a href="#">
           <h2 className="text-start font-bold text-xl mb-2 line-clamp-2 hover:underline">
-            {props.title}
+            {title}
           </h2>
         </a>
         <p className="text-muted-foreground text-sm mb-4 flex-grow line-clamp-3 text-start">
-          {props.description}
+          {description}
         </p>
         <div className="flex items-center text-sm">
           <img
@@ -124,9 +145,9 @@ export function BlogCard(props) {
             src="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449784/my-blog-post/xgfy0xnvyemkklcqodkg.jpg"
             alt="Tomson P."
           />
-          <span>{props.author}</span>
+          <span>{author}</span>
           <span className="mx-2 text-gray-300">|</span>
-          <span>{props.date}</span>
+          <span>{date}</span>
         </div>
       </div>
     </div>
