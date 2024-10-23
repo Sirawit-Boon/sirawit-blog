3d957c7d20a6e26aefe793e1f22e1d7e368d4007
Sirawit-Boon <ter.sirawit1997@gmail.com>
2024-10-23 17:27:42 +0700

    feat: add fiterfn on button

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -1,24 +1,45 @@
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
 import { blogPosts } from "../data/blogPost";
+import { useState } from "react";
 
 export function ArticleSection() {
   const categories = ["Highlight", "Cat", "Inspiration", "General"];
+  const [filteredCategories, setFilteredCategories] = useState(blogPosts);
+  const filteredCat = (category) => {
+    const newItems = blogPosts.filter((data) => data.category === category)
+    setFilteredCategories(newItems);
+  };
+
+  const buttonElements = categories.slice(1).map((categorie, index) => {
+    return (
+      <button
+        key={index}
+        onClick={() => {
+          filteredCat(categorie)
+          className=""
+        }}
+        className="px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[muted]"
+      >
+        {categorie}
+      </button>
+    );
+  });
+
+  const buttonAllElements = () => {
+    setFilteredCategories(blogPosts)
+  }
+
   return (
     <section className="latest-article gap-[80px] flex bg-[#F9F8F6] flex-col justify-center items-center">
       <div className="article-header gap-[32px] h-[144px] flex flex-col w-full">
         <h3 className="text-header text-[24px] text-left">Latest articles</h3>
         <div className="article-tabbar flex w-full h-[80px] justify-between items-center bg-[#EFEEEB] rounded-xl px-10">
           <div className="hidden md:flex space-x-2">
-            <button className="px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[#DAD6D1]">
+            <button onClick={buttonAllElements} className="px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[#DAD6D1]">
               Highlight
             </button>
-            {categories.slice(1).map((categorie, index) => {
-              return <button key={index} className="px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[muted]">
-                {categorie}
-              </button>;
-            })}
+            {buttonElements}
           </div>
           <div className="input-search relative w-[360px]">
             <Input
@@ -34,72 +55,16 @@ export function ArticleSection() {
         </div>
       </div>
       <div className="blog-card-content grid grid-cols-2 gap-x-4 gap-y-4 max-w-[1200px]">
-        {/* <div>
-          <BlogCard
-            title={blogPosts[0].title}
-            image={blogPosts[0].image}
-            category={blogPosts[0].category}
-            description={blogPosts[0].description}
-            author={blogPosts[0].author}
-            date={blogPosts[0].date}
-          />
-        </div>
-        <BlogCard
-          title={blogPosts[1].title}
-          image={blogPosts[1].image}
-          category={blogPosts[1].category}
-          description={blogPosts[1].description}
-          author={blogPosts[1].author}
-          date={blogPosts[1].date}
-        />
-        <div>
-          <BlogCard
-            title={blogPosts[2].title}
-            image={blogPosts[2].image}
-            category={blogPosts[2].category}
-            description={blogPosts[2].description}
-            author={blogPosts[2].author}
-            date={blogPosts[2].date}
-          />
-        </div>
-        <BlogCard
-          title={blogPosts[3].title}
-          image={blogPosts[3].image}
-          category={blogPosts[3].category}
-          description={blogPosts[3].description}
-          author={blogPosts[3].author}
-          date={blogPosts[3].date}
-        />
-        <div>
-          <BlogCard
-            title={blogPosts[4].title}
-            image={blogPosts[4].image}
-            category={blogPosts[4].category}
-            description={blogPosts[4].description}
-            author={blogPosts[4].author}
-            date={blogPosts[4].date}
-          />
-        </div>
-        <div>
-          <BlogCard
-            title={blogPosts[5].title}
-            image={blogPosts[5].image}
-            category={blogPosts[5].category}
-            description={blogPosts[5].description}
-            author={blogPosts[5].author}
-            date={blogPosts[5].date}
-          />
-        </div> */}
-        {blogPosts.map((key, index) => {
+        {filteredCategories.map((key, index) => {
           return (
-            <BlogCard 
-            key={index}
-            image={key.image}
-            title={key.title}
-            category={key.category}
-            description={key.description}
-            date={key.date}
-            author={key.author}
+            <BlogCard
+              key={index}
+              image={key.image}
+              title={key.title}
+              category={key.category}
+              description={key.description}
+              date={key.date}
+              author={key.author}
             />
           );
         })}
